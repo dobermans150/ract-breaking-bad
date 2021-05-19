c1a9392da85218b112289229a25d3cdf54d7a524
Christian Guevara <christianjguevara@hotmail.com>
2021-05-19 17:47:25 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Frase from './components/Frase';
 
-function App() {
+
+export default function App() {
+
+  const [frase,obtenerFrase] = useState({});
+
+  /* Consulta a una rest api */
+  const consultarAPI = async ()=>{
+    const resultado = await axios('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    
+    //console.log(resultado.data[0]);
+    /* agregar el resultado de la API al state; (similar a this.useState) */
+
+    obtenerFrase(resultado.data[0]);
+    
+  }
+
+  useEffect(()=>{
+    consultarAPI();
+  }, [])
+  
+  //console.log(frase);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <Frase
+        frase={frase}
+      />
+      <button onClick={consultarAPI}>Generar Nueva</button>
     </div>
-  );
+  )
 }
-
-export default App;
